a47c8175aeb10cf3388b9f6fe719d7fc3b363cb6
Far4Ru <faty422@mail.ru>
2023-02-28 16:13:18 +0300

    Добавлены комментарии в задание 3

--- practice_1_2/task3.py
+++ practice_1_2/task3.py
@@ -1,4 +1,12 @@
 def is_leap_year(year):
+    """
+    Функция определения високосного года
+
+    :param year: Год
+    :type year: int
+    :return: Истинность определения високосного года
+    :rtype: bool
+    """
     if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
         return True
     else:
@@ -6,6 +14,14 @@ def is_leap_year(year):
 
 
 def is_leap_year_result(year):
+    """
+    Функция результата определение високосного года
+
+    :param year: Год
+    :type year: int
+    :return: Результат определения високосного года
+    :rtype: str
+    """
     if is_leap_year(year):
         return "Год " + str(year) + " - високосный"
     else:
