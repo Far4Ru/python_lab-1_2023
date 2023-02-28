fe70526d1962a098934fc07cfd4d6776d1bb17c7
Sasha <far4ru@gmail.com>
2023-02-28 15:55:26 +0300

    Задание 4

---
+++ practice_1_2/task4.py
@@ -0,0 +1,31 @@
+COLORS = ['красный', 'синий', 'желтый']
+MIX_COLORS = ['фиолетовый', 'оранжевый', 'зеленый']
+
+
+def mix_colors(fc, sc):
+    """
+    Функция вывода цвета, полученного в результате смешения двух цветов
+
+    :param fp: Первый цвет
+    :type fp: str
+    :param sp: Второй цвет
+    :type fp: str
+    :return: Полученный цвет
+    :rtype: str
+    :raises ValueError: Неверный цвет
+    """
+    fc = fc.lower()
+    sc = sc.lower()
+    if fc not in COLORS or sc not in COLORS:
+        raise ValueError('Введен неверный цвет')
+    else:
+        if (fc == COLORS[0] and sc == COLORS[1]) or (sc == COLORS[0] and fc == COLORS[1]):
+            return MIX_COLORS[0]
+        elif (fc == COLORS[0] and sc == COLORS[2]) or (sc == COLORS[0] and fc == COLORS[2]):
+            return MIX_COLORS[1]
+        elif (fc == COLORS[1] and sc == COLORS[2]) or (sc == COLORS[1] and fc == COLORS[2]):
+            return MIX_COLORS[2]
+
+
+if __name__ == '__main__':
+    print(mix_colors(input("Первый цвет\n"), input("Второй цвет\n")))
