8724d301a7bc83b52bbb0b2800bc14421766a4d5
Sasha <faty422@mail.ru>
2023-04-23 15:56:38 +0300

    Обновлена 4 задача

--- practice_5/task4.py
+++ practice_5/task4.py
@@ -1,22 +1,56 @@
-from random import random
+from random import sample
 
 
-def task4():
-    input()
-    mv5 = ("Иванов", "Панин", "Юрина", "Королёва", "Шелькова", "Бекренева")
-    mv4 = ("Весёлова", "Горбачева", "Прохоренкова", "Коллар", "Столетова", "Шевцова")
-    mv_sport = tuple(random.sample(mv5, 6)[:3]+random.sample(mv4, 6)[:3])
-    print(mv5)
-    print(mv4)
-    print(*mv_sport)
-    print("Кол - во студентов", len(mv_sport))
-    mv_sport = tuple(sorted(list(mv_sport)))
-    print(*mv_sport)
-    if "Иванов" in mv_sport:
-        print(list(mv_sport).count("Иванов"), " - Иванов присутствует! ")
+def init_groups():
+    """
+    Создается два списка: один из 10 фамилий студентов одной группы, другой из 10 фамилий студентов другой группы.
+    """
+    mv5 = ("Иванов", "Стиценко", "Юрина", "Бекренёва", "Королёва", "Шелькова", "Семёнов", "Рословец", "Пузанов", "Панин")
+    mv4 = ("Веселова", "Соколов", "Прохоренкова", "Коллар", "Белоцерковский", "Шевцова", "Николаева", "Родионова", "Крупницкий")
+    # print(group1)
+    # print(group2)
+    return {"mv5": mv5, "mv4": mv4}
+
+
+def make_sport_group(group1, group2):
+    """
+    Создается спортивная команда (один кортеж) по 5 любых студентов из каждой группы.
+    """
+    # sample() - случайная выборка нескольких элементов последовательности без замены.
+    group = tuple(sample(group1, len(group1))[:5] + sample(group2, len(group2))[:5])
+    # print(*group)
+    return group
+
+
+def sport_group_len(group):
+    """
+     Выводится длина кортежа спортивной команды.
+    """
+    print("Студентов", len(group))
+
+
+def sport_sort(group):
+    """
+    Сортируется кортеж по алфавиту.
+    """
+    sorted_group = tuple(sorted(list(group)))
+    # print(*group)
+    return sorted_group
+
+
+def sport_search(name, group):
+    """
+    Определяется, входит ли в полученную команду студент "Иванов". И сколько раз встречается эта фамилия в кортеже.
+    """
+    if name in group:
+        print(list(group).count(name), name + " присутствует")
     else:
-        print("Иванов, опять прогуливает!")
+        print(name + " отсутствует")
 
 
 if __name__ == '__main__':
-    task4()
+    groups = init_groups()
+    sport_group = make_sport_group(groups['mv5'], groups['mv4'])
+    sport_group_len(sport_group)
+    sorted_sport_group = sport_sort(sport_group)
+    sport_search("Иванов", sorted_sport_group)
