1fed94605aa03a9e30ee6fd9d7b11f45a0e6cfdc
Sasha <faty422@mail.ru>
2023-04-23 16:55:24 +0300

    Добавлена 2 задача

--- practice_6/task2.py
+++ practice_6/task2.py
@@ -1,6 +1,23 @@
-def task2():
-    pass
+def get_cost(word):
+    """
+    Вычисление стоимости введенного слова.
+    """
+    letter_prices = {
+        "авеинорст": 1,
+        "дклмуп": 2,
+        "бгёья": 3,
+        "йы": 4,
+        "жзхцч": 5,
+        "шэю": 8,
+        "фщъ": 10
+    }
+    cost = 0
+    for letter in word:
+        for letters, price in letter_prices.items():
+            if letter.lower() in letters:
+                cost += price
+    print("Стоимость слова:", cost)
 
 
 if __name__ == '__main__':
-    task2()
+    get_cost(input("Введите слово: "))
