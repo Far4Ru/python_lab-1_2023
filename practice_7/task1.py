c17af6f4d53fddbcb6d234651390619155d3076d
Sasha <faty422@mail.ru>
2023-04-23 17:56:02 +0300

    Добавлена 1 задача

--- practice_7/task1.py
+++ practice_7/task1.py
@@ -1,4 +1,28 @@
-"""
-Подготовьте любой графический файл для выполнения практической работы. Напишите программу, которая открывает и выводит этот файл на экран.
-Получите и выведите в консоль информацию о размере изображения, его формате, его цветовой модели.
-"""
+from PIL import Image
+
+
+def open_image(filename):
+    """
+    Открытие и вывод файла на экран и вывод его информации в консоль.
+    """
+    with Image.open(filename) as image:
+        image.load()
+        get_image_info(image)
+        image.show()
+
+
+def get_image_info(image):
+    """
+    Вывод информации о размере изображения, его формате и его цветовой модели.
+    """
+    width, height = image.size
+    image_format = image.format
+    image_mode = image.mode
+    print("Ширина:", width)
+    print("Высота:", height)
+    print("Формат:", image_format)
+    print("Цветовая модель:", image_mode)
+
+
+if __name__ == '__main__':
+    open_image("images/task1.png")
