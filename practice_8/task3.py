6249c6df95103122fd7b322aef99e769d32c0c7c
Sasha <faty422@mail.ru>
2023-04-24 00:15:05 +0300

    Добавлена 3 задача

--- practice_8/task3.py
+++ practice_8/task3.py
@@ -1,18 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-name = input("Введите имя получателя: ")
-filename = "postcard.jpg"
-with Image.open(filename) as img:
-    img.load()
-# используется файл с жирными символами шрифта Arial
-font = ImageFont.truetype("arial_bold.ttf", 25)
-draw_text = ImageDraw.Draw(img)
-draw_text.text(
-    (img.width // 2 - 100, 15),
-    name + ", поздравляю!",
-    font=font,
-    fill=('#FAACAC')
-)
-img.show()
-img.save(name + "_postcard.png")
+def name_to_postcard(name, filename):
+    """
+    Добавление на заданную открытку текста «<имя>, поздравляю!»
+    """
+
+    with Image.open("images/" + filename) as image:
+        image.load()
+
+    # используется файл с жирными символами шрифта Arial
+    font = ImageFont.truetype("fonts/arial_bold.ttf", 25)
+    draw_text = ImageDraw.Draw(image)
+    draw_text.text(
+        (image.width // 2, image.height // 2 - 155),
+        name + ", поздравляю!",
+        font=font,
+        fill='#FFFFFF'
+    )
+    image.show()
+    image.save(name + "_postcard.png")
+
+
+if __name__ == '__main__':
+    name_to_postcard(input("Введите имя получателя: "), "task2_4.jpg")
